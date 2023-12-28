64b7a3cd0ea15557939c22af687f41ca4ddd8ac8
letusget <a14705129921@163.com>
2023-12-28 08:07:21 +0800

    调整格式

--- new_threadPool_cpp/include/TaskQueue.h
+++ new_threadPool_cpp/include/TaskQueue.h
@@ -7,36 +7,31 @@
 #include <thread>
 
 //任务队列
-template <typename T>
-class TaskQueue
-{
+template <typename T> class TaskQueue {
 private:
-    std::queue<Task<T>> m_taskQ;
+  std::queue<Task<T>> m_taskQ;
 
-    //使用 互斥锁，防止同时访问冲突
-    pthread_mutex_t m_mutex;
+  //使用 互斥锁，防止同时访问冲突
+  pthread_mutex_t m_mutex;
 
 public:
-    TaskQueue();
+  TaskQueue();
 
-    //添加任务
-    void addTask(Task<T> task);
-    void addTask(callBack f,void* arg);
+  //添加任务
+  void addTask(Task<T> task);
+  void addTask(callBack f, void *arg);
 
-    //取出一个任务 -直接操作队头即可
-    Task<T> takeTask();
+  //取出一个任务 -直接操作队头即可
+  Task<T> takeTask();
 
-    //获取当前任务数量
-    inline size_t taskNumber()  //与返回值的类型一致
-    {
-        //使用内联函数，提高效率，直接获取任务个数
-        return m_taskQ.size();
-    }
+  //获取当前任务数量
+  inline size_t taskNumber() //与返回值的类型一致
+  {
+    //使用内联函数，提高效率，直接获取任务个数
+    return m_taskQ.size();
+  }
 
-    ~TaskQueue();
+  ~TaskQueue();
 };
-
-
-
 
 #endif
--- new_threadPool_cpp/src/main.cpp
+++ new_threadPool_cpp/src/main.cpp
@@ -1,50 +1,48 @@
 #include "Task.h"
-#include "threadPool.h"
-#include "threadPool.cpp"   //由于模板类的声明和实现是分开的，所以需要包含源文件，否则就会报错“未定义的引用”
 #include "TaskQueue.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <pthread.h>
-#include <unistd.h>
-#include <string.h>
+#include "threadPool.cpp" //由于模板类的声明和实现是分开的，所以需要包含源文件，否则就会报错“未定义的引用”
+#include "threadPool.h"
 #include <iostream>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 using namespace std;
 
 //工作函数
-void taskFunction(void* arg)
-{
-    //需要对传入的地址进行类型转换，然后再解引用，得到该地址保存的数据
-    int num=*(int*)arg;
-   // printf("thread %ld is working, number = %d \n",pthread_self(),num);
-    cout<<"thread "<<pthread_self()<<" is working, number= "<<num<<"\n";
-    sleep(1);
+void taskFunction(void *arg) {
+  //需要对传入的地址进行类型转换，然后再解引用，得到该地址保存的数据
+  int num = *(int *)arg;
+  // printf("thread %ld is working, number = %d \n",pthread_self(),num);
+  cout << "thread " << pthread_self() << " is working, number= " << num << "\n";
+  sleep(1);
 }
 
-int main()
-{
-    cout<<"threadpool 向你问好! "<<"\n";
+int main() {
+  cout << "threadpool 向你问好! "
+       << "\n";
 
-    int minThread=3;
-    int maxThread=10;
-    int tasks=100;
-    //创建线程池
-    ThreadPool<int> pool(minThread,maxThread);   //当线程执行完毕会自动析构，所以这里就不再需要主动调用析构函数了
+  int minThread = 3;
+  int maxThread = 10;
+  int tasks = 100;
+  //创建线程池
+  ThreadPool<int> pool(
+      minThread,
+      maxThread); //当线程执行完毕会自动析构，所以这里就不再需要主动调用析构函数了
 
-    //添加100个任务
+  //添加100个任务
+  //使用堆 内存
+  // int * num=(int *)malloc(sizeof(int));
+  for (int i = 0; i < tasks; i++) {
     //使用堆 内存
-    //int * num=(int *)malloc(sizeof(int));
-    for(int i=0;i<tasks;i++)
-    {
-        //使用堆 内存
-        int * num=new int(i+100);   //这里的空间在work中释放了，不存在内存泄露
-        pool.addTask(Task<int>(taskFunction,num));
-    }
+    int *num = new int(i + 100); //这里的空间在work中释放了，不存在内存泄露
+    pool.addTask(Task<int>(taskFunction, num));
+  }
 
-    //保证工作线程已经处理完毕
-    sleep(20);
+  //保证工作线程已经处理完毕
+  sleep(20);
 
-    
-    return 0;
+  return 0;
 }
-
